e96d48b7fa195a54c145852a7e09107fdc1572c9
Bart Milo <bartlomiej.milosz@gmail.com>
2024-05-21 20:56:47 +0200

    Refactor common classes and delete redundant IndexOutOfBound exception in controller advice

--- src/main/java/io/bartmilo/student/enrolment/app/handler/error/ErrorHandler.java
+++ src/main/java/io/bartmilo/student/enrolment/app/handler/error/ErrorHandler.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ErrorHandler implements ErrorController {
 
-    @GetMapping("/error")
-    public String handleError() {
-        // This will forward to /error.html without changing the URL in the browser
-        return "forward:/error.html";
-    }
+  @GetMapping("/error")
+  public String handleError() {
+    // This will forward to /error.html without changing the URL in the browser
+    return "forward:/error.html";
+  }
 }
