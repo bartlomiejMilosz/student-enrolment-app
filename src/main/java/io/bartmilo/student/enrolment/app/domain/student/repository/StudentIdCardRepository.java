37e47135c464baa34353d4821001418ba3c9e6e4
Bart Milo <bartlomiej.milosz@gmail.com>
2024-05-22 13:18:04 +0200

    Simplify student repositories by adding JpaRepository directly

--- src/main/java/io/bartmilo/student/enrolment/app/domain/student/repository/StudentIdCardRepository.java
+++ src/main/java/io/bartmilo/student/enrolment/app/domain/student/repository/StudentIdCardRepository.java
@@ -1,12 +1,8 @@
 package io.bartmilo.student.enrolment.app.domain.student.repository;
 
 import io.bartmilo.student.enrolment.app.domain.student.model.StudentIdCardEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentIdCardRepository extends
-        CrudRepository<StudentIdCardEntity, Long>,
-        PagingAndSortingRepository<StudentIdCardEntity, Long> {
-}
+public interface StudentIdCardRepository extends JpaRepository<StudentIdCardEntity, Long> {}
--- src/main/java/io/bartmilo/student/enrolment/app/domain/student/repository/StudentRepository.java
+++ src/main/java/io/bartmilo/student/enrolment/app/domain/student/repository/StudentRepository.java
@@ -1,37 +1,31 @@
 package io.bartmilo.student.enrolment.app.domain.student.repository;
 
 import io.bartmilo.student.enrolment.app.domain.student.model.StudentEntity;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface StudentRepository extends JpaRepository<StudentEntity, Long> {
-    @Query("SELECT s FROM Student s WHERE s.email = ?1")
-    Optional<StudentEntity> findStudentByEmail(String email);
+  @Query("SELECT s FROM Student s WHERE s.email = ?1")
+  Optional<StudentEntity> findStudentByEmail(String email);
 
-    @Query("SELECT s FROM Student s WHERE s.firstName = ?1 AND s.age = ?2")
-    List<StudentEntity> findStudentsByFirstNameAndAgeEquals(String firstName, Integer age);
+  @Query("SELECT s FROM Student s WHERE s.firstName = ?1 AND s.age = ?2")
+  List<StudentEntity> findStudentsByFirstNameAndAgeEquals(String firstName, Integer age);
 
-    @Query(
-            value = "SELECT * FROM student WHERE first_name = ?1 AND age >= ?2",
-            nativeQuery = true
-    )
-    List<StudentEntity> findStudentsByFirstNameAndAgeGreaterOrEqualNative(String firstName, Integer age);
+  @Query(value = "SELECT * FROM student WHERE first_name = ?1 AND age >= ?2", nativeQuery = true)
+  List<StudentEntity> findStudentsByFirstNameAndAgeGreaterOrEqualNative(
+      String firstName, Integer age);
 
-    @Query(
-            value = "SELECT * FROM student WHERE first_name = :firstName AND age >= :age",
-            nativeQuery = true
-    )
-    List<StudentEntity> findStudentsByFirstNameAndAgeGreaterOrEqualNative_NamedParameters(
-            @Param("firstName") String firstName,
-            @Param("age") Integer age
-    );
+  @Query(
+      value = "SELECT * FROM student WHERE first_name = :firstName AND age >= :age",
+      nativeQuery = true)
+  List<StudentEntity> findStudentsByFirstNameAndAgeGreaterOrEqualNative_NamedParameters(
+      @Param("firstName") String firstName, @Param("age") Integer age);
 
-    @Query("SELECT s FROM Student s WHERE s.lastName LIKE ?1")
-    List<StudentEntity> findByLastNameStartingWith(String prefix);
+  @Query("SELECT s FROM Student s WHERE s.lastName LIKE ?1")
+  List<StudentEntity> findByLastNameStartingWith(String prefix);
 }
