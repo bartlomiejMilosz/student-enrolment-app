0b8f96c9afa9d7c23eacd61f511b4ac8fe8c6f03
Bart Milo <bartlomiej.milosz@gmail.com>
2024-05-22 08:19:21 +0200

    Refactor book service interface to work only on DTO objects

--- src/main/java/io/bartmilo/student/enrolment/app/domain/book/service/BookService.java
+++ src/main/java/io/bartmilo/student/enrolment/app/domain/book/service/BookService.java
@@ -1,5 +1,6 @@
 package io.bartmilo.student.enrolment.app.domain.book.service;
 
+import io.bartmilo.student.enrolment.app.domain.book.model.BookDto;
 import io.bartmilo.student.enrolment.app.domain.book.model.BookEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,10 +12,10 @@ public interface BookService {
     /**
      * Saves the specified book to the database.
      *
-     * @param bookEntity The book to save to database.
+     * @param bookDto The book to save to database.
      * @return The saved book.
      */
-    BookEntity save(BookEntity bookEntity);
+    BookDto save(BookDto bookDto);
 
     /**
      * Returns all books in the database.
@@ -22,7 +23,7 @@ public interface BookService {
      * @param pageable The pagination information.
      * @return A paginated list of books in the database.
      */
-    Page<BookEntity> findAll(Pageable pageable);
+    Page<BookDto> findAll(Pageable pageable);
 
     /**
      * Returns the book with the specified id.
@@ -30,7 +31,7 @@ public interface BookService {
      * @param id ID of the book to retrieve.
      * @return The requested book if found.
      */
-    Optional<BookEntity> findById(Long id);
+    BookDto findById(Long id);
 
     /**
      * Returns boolean of book in the database with the specified ID. Checks whether the book exists in the database.
@@ -43,10 +44,10 @@ public interface BookService {
      * Updates the specified book, identified by given ID.
      *
      * @param id         The ID of the book to update.
-     * @param bookEntity The book to update.
+     * @param bookDto The book to update.
      * @return The updated book.
      */
-    BookEntity partialUpdate(Long id, BookEntity bookEntity);
+    BookDto partialUpdate(Long id, BookDto bookDto);
 
     /**
      * Deletes the book with the specified ID.
