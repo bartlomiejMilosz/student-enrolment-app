14c263c2851ce572f3466900e18195b119b97d9c
Bart Milo <bartlomiej.milosz@gmail.com>
2024-05-22 11:12:22 +0200

    Simplify repository by extending JpaRepository directly

--- src/main/java/io/bartmilo/student/enrolment/app/domain/rental/repository/RentalRepository.java
+++ src/main/java/io/bartmilo/student/enrolment/app/domain/rental/repository/RentalRepository.java
@@ -1,12 +1,8 @@
 package io.bartmilo.student.enrolment.app.domain.rental.repository;
 
 import io.bartmilo.student.enrolment.app.domain.rental.model.RentalEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RentalRepository extends
-        CrudRepository<RentalEntity, Long>,
-        PagingAndSortingRepository<RentalEntity, Long> {
-}
+public interface RentalRepository extends JpaRepository<RentalEntity, Long> {}
