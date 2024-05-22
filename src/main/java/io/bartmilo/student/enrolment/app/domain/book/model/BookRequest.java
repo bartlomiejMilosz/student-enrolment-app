556b7c1a16477d3e83a890ccb359de9ed1f34715
Bart Milo <bartlomiej.milosz@gmail.com>
2024-05-22 08:16:56 +0200

    Add book request and response model to achieve more encapsulated mapping

---
+++ src/main/java/io/bartmilo/student/enrolment/app/domain/book/model/BookRequest.java
@@ -0,0 +1,13 @@
+package io.bartmilo.student.enrolment.app.domain.book.model;
+
+import java.time.LocalDateTime;
+import lombok.*;
+
+@Builder
+public record BookRequest(
+    Long id,
+    String bookAuthor,
+    String title,
+    String isbn,
+    LocalDateTime createdAt,
+    Integer stock) {}
