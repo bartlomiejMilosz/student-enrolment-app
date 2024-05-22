8f2ee2ab44b8098bbc1761980d8a2af61df45163
Bart Milo <bartlomiej.milosz@gmail.com>
2024-05-22 13:20:14 +0200

    Refactor services implementations and add mapping

--- src/main/java/io/bartmilo/student/enrolment/app/domain/student/service/StudentIdCardServiceImpl.java
+++ src/main/java/io/bartmilo/student/enrolment/app/domain/student/service/StudentIdCardServiceImpl.java
@@ -1,35 +1,41 @@
 package io.bartmilo.student.enrolment.app.domain.student.service;
 
-import io.bartmilo.student.enrolment.app.domain.student.model.StudentEntity;
-import io.bartmilo.student.enrolment.app.domain.student.model.StudentIdCardEntity;
+import io.bartmilo.student.enrolment.app.domain.student.model.*;
 import io.bartmilo.student.enrolment.app.domain.student.repository.StudentIdCardRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.bartmilo.student.enrolment.app.util.DomainMapper;
+import java.util.Random;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StudentIdCardServiceImpl implements StudentIdCardService {
-    private final StudentIdCardRepository studentIdCardRepository;
+  private final StudentIdCardRepository studentIdCardRepository;
+  private final Random random;
+  private final DomainMapper domainMapper;
 
-    @Autowired
-    public StudentIdCardServiceImpl(StudentIdCardRepository studentIdCardRepository) {
-        this.studentIdCardRepository = studentIdCardRepository;
-    }
+  public StudentIdCardServiceImpl(
+      StudentIdCardRepository studentIdCardRepository, Random random, DomainMapper domainMapper) {
+    this.studentIdCardRepository = studentIdCardRepository;
+    this.random = random;
+    this.domainMapper = domainMapper;
+  }
 
-    @Override
-    @Transactional
-    public StudentIdCardEntity generateStudentIdCard(StudentEntity studentEntity) {
-        StudentIdCardEntity studentIdCard = StudentIdCardEntity.builder()
-                .cardNumber(generateCardNumber())
-                .status(StudentIdCardEntity.CardStatus.ACTIVE)
-                .studentEntity(studentEntity)
-                .build();
+  @Override
+  @Transactional
+  public StudentIdCardDto generateStudentIdCard(StudentDto studentDto) {
+    var studentEntity = domainMapper.convertEntityToDto(studentDto, StudentEntity.class);
+    var studentIdCard =
+        StudentIdCardEntity.builder()
+            .cardNumber(generateCardNumber())
+            .status(IdCardStatus.ACTIVE)
+            .studentEntity(studentEntity)
+            .build();
 
-        return studentIdCardRepository.save(studentIdCard);
-    }
+    var savedStudentIdCardEntity = studentIdCardRepository.save(studentIdCard);
+    return domainMapper.convertEntityToDto(savedStudentIdCardEntity, StudentIdCardDto.class);
+  }
 
-    private String generateCardNumber() {
-        // Implement your logic to generate a unique card number here
-        return "CARD" + (int)(Math.random() * 1000000);
-    }
+  private String generateCardNumber() {
+    return "CARD" + (random.nextInt() * 1000000);
+  }
 }
--- src/main/java/io/bartmilo/student/enrolment/app/domain/student/service/StudentServiceImpl.java
+++ src/main/java/io/bartmilo/student/enrolment/app/domain/student/service/StudentServiceImpl.java
@@ -1,94 +1,104 @@
 package io.bartmilo.student.enrolment.app.domain.student.service;
 
+import io.bartmilo.student.enrolment.app.domain.student.exception.StudentNotFoundException;
+import io.bartmilo.student.enrolment.app.domain.student.model.StudentDto;
 import io.bartmilo.student.enrolment.app.domain.student.model.StudentEntity;
+import io.bartmilo.student.enrolment.app.domain.student.model.StudentIdCardEntity;
 import io.bartmilo.student.enrolment.app.domain.student.repository.StudentRepository;
+import io.bartmilo.student.enrolment.app.util.DomainMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
 @Service
 public class StudentServiceImpl implements StudentService {
-
     private static final Logger LOGGER = LoggerFactory.getLogger(StudentServiceImpl.class);
-
     private final StudentRepository studentRepository;
     private final StudentIdCardService studentIdCardService;
+    private final DomainMapper domainMapper;
 
-    @Autowired
-    public StudentServiceImpl(
-            StudentRepository studentRepository,
-            StudentIdCardService studentIdCardService
-    ) {
+    public StudentServiceImpl(StudentRepository studentRepository, StudentIdCardService studentIdCardService, DomainMapper domainMapper) {
         this.studentRepository = studentRepository;
         this.studentIdCardService = studentIdCardService;
+        this.domainMapper = domainMapper;
     }
 
     @Override
     @Transactional
-    public StudentEntity save(StudentEntity student) {
-        var idCard = studentIdCardService.generateStudentIdCard(student);
-        student.setStudentIdCardEntity(idCard);
-        LOGGER.info("Save student to the database: {}", student);
+    public StudentDto save(StudentDto studentDto) {
+        LOGGER.info("Saving student to the database: {}", studentDto);
+        var studentIdCardDto = studentIdCardService.generateStudentIdCard(studentDto);
+        var studentIdCardEntity = domainMapper.convertDtoToEntity(studentIdCardDto, StudentIdCardEntity.class);
+        var studentEntity = domainMapper.convertDtoToEntity(studentDto, StudentEntity.class);
+        studentEntity.setStudentIdCardEntity(studentIdCardEntity);
 
-        // TODO: keep it but consider
-        if (student.getId() != null && findById(student.getId()).isPresent()) {
-            return studentRepository.saveAndFlush(student); // Ensure merge behavior
-        } else {
-            return studentRepository.save(student); // New entity
-        }
+        var savedStudentEntity = studentRepository.save(studentEntity);
+        return domainMapper.convertEntityToDto(savedStudentEntity, StudentDto.class);
     }
 
     @Override
     @Transactional(readOnly = true)
-    public Page<StudentEntity> findAll(Pageable pageable) {
+    public Page<StudentDto> findAll(Pageable pageable) {
         LOGGER.info("Fetching all students with pagination");
-        return studentRepository.findAll(pageable);
+        var studentEntityPage = studentRepository.findAll(pageable);
+        return studentEntityPage.map(entity -> domainMapper.convertEntityToDto(entity, StudentDto.class));
     }
 
     @Override
     @Transactional(readOnly = true)
-    public Optional<StudentEntity> findById(Long id) {
-        LOGGER.info("Find student with id: {}", id);
-        return studentRepository.findById(id);
+    public StudentDto findById(Long id) {
+        LOGGER.info("Finding student with ID: {}", id);
+        return studentRepository
+                .findById(id)
+                .map(studentEntity -> domainMapper.convertEntityToDto(studentEntity, StudentDto.class))
+                .orElseThrow(() -> new StudentNotFoundException("Student not found with ID: " + id));
     }
 
     @Override
     @Transactional(readOnly = true)
-    public boolean isExists(Long id) {
-        LOGGER.info("Check whether the student exists with id: {}", id);
-        return !studentRepository.existsById(id);
+    public boolean exists(Long id) {
+        LOGGER.info("Checking whether the student exists with ID: {}", id);
+        return studentRepository.existsById(id);
     }
 
     @Override
     @Transactional
-    public StudentEntity partialUpdate(Long id, StudentEntity studentEntity) {
-        LOGGER.info("Make sure the student you want to update has specified id: {}", id);
-        studentEntity.setId(id);
-
-        LOGGER.info("Update student: {}, with id: {}", studentEntity, id);
-        return studentRepository.findById(id).map(existingAuthor -> {
-            Optional.ofNullable(studentEntity.getFirstName())
-                    .ifPresent(existingAuthor::setFirstName);
-            Optional.ofNullable(studentEntity.getLastName())
-                    .ifPresent(existingAuthor::setLastName);
-            Optional.ofNullable(studentEntity.getEmail())
-                    .ifPresent(existingAuthor::setEmail);
-            Optional.ofNullable(studentEntity.getAge())
-                    .ifPresent(existingAuthor::setAge);
-            return studentRepository.save(existingAuthor);
-        }).orElseThrow(() -> new RuntimeException("Student does not exist"));
+    public StudentDto partialUpdate(Long id, StudentDto studentDto) {
+        LOGGER.info("Updating student with ID: {}", id);
+        return studentRepository
+                .findById(id)
+                .map(existingStudent -> updateExistingStudent(studentDto, existingStudent))
+                .orElseThrow(() -> new StudentNotFoundException("Student not found with ID: " + id));
     }
 
     @Override
     @Transactional
     public void delete(Long id) {
-        LOGGER.info("Delete student with id: {}", id);
+        LOGGER.info("Deleting student with ID: {}", id);
         studentRepository.deleteById(id);
+    }
+
+    private StudentDto updateExistingStudent(StudentDto studentDto, StudentEntity existingStudent) {
+        LOGGER.info("Found student to update: {}", existingStudent);
+
+        if (studentDto.firstName() != null) {
+            existingStudent.setFirstName(studentDto.firstName());
+        }
+        if (studentDto.lastName() != null) {
+            existingStudent.setLastName(studentDto.lastName());
+        }
+        if (studentDto.email() != null) {
+            existingStudent.setEmail(studentDto.email());
+        }
+        if (studentDto.age() != null) {
+            existingStudent.setAge(studentDto.age());
+        }
+
+        var updatedStudent = studentRepository.save(existingStudent);
+        LOGGER.info("Updated student: {}", updatedStudent);
+        return domainMapper.convertEntityToDto(updatedStudent, StudentDto.class);
     }
 }
