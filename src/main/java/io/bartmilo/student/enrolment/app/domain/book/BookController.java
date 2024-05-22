fb48219a89b75dc618589c5d006e24523ad005a5
Bart Milo <bartlomiej.milosz@gmail.com>
2024-05-22 08:20:13 +0200

    Refactor book service implementation and controller by adding more specialized mapping

--- src/main/java/io/bartmilo/student/enrolment/app/domain/book/BookController.java
+++ src/main/java/io/bartmilo/student/enrolment/app/domain/book/BookController.java
@@ -1,130 +1,92 @@
 package io.bartmilo.student.enrolment.app.domain.book;
 
 import io.bartmilo.student.enrolment.app.domain.book.model.BookDto;
-import io.bartmilo.student.enrolment.app.domain.book.model.BookEntity;
-import io.bartmilo.student.enrolment.app.util.ModelMapper;
+import io.bartmilo.student.enrolment.app.domain.book.model.BookRequest;
+import io.bartmilo.student.enrolment.app.domain.book.model.BookResponse;
 import io.bartmilo.student.enrolment.app.domain.book.service.BookService;
+import io.bartmilo.student.enrolment.app.util.DomainMapper;
+import java.net.URI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(BookController.class);
-    private final BookService bookService;
-    private final ModelMapper<BookEntity, BookDto> bookModelMapper;
+  private static final Logger LOGGER = LoggerFactory.getLogger(BookController.class);
+  private final BookService bookService;
+  private final DomainMapper domainMapper;
 
-    @Autowired
-    public BookController(BookService bookService, ModelMapper<BookEntity, BookDto> bookModelMapper) {
-        this.bookService = bookService;
-        this.bookModelMapper = bookModelMapper;
+  public BookController(BookService bookService, DomainMapper domainMapper) {
+    this.bookService = bookService;
+    this.domainMapper = domainMapper;
+  }
+
+  @GetMapping
+  public ResponseEntity<Page<BookResponse>> getAllBooks(
+      @PageableDefault(size = 10) Pageable pageable) {
+    LOGGER.info("Request to get all books");
+    var bookDtoPage = bookService.findAll(pageable);
+    LOGGER.info("Books retrieved with pagination");
+    var bookResponsePage =
+        bookDtoPage.map(bookDto -> domainMapper.convertDtoToResponse(bookDto, BookResponse.class));
+    return ResponseEntity.ok(bookResponsePage);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<BookResponse> getBookById(@PathVariable Long id) {
+    LOGGER.info("Request to get book by ID: {}", id);
+    var bookDto = bookService.findById(id);
+    LOGGER.info("Book retrieved successfully: {}", bookDto);
+    var bookResponse = domainMapper.convertDtoToResponse(bookDto, BookResponse.class);
+    return ResponseEntity.ok(bookResponse);
+  }
+
+  @PostMapping
+  public ResponseEntity<BookResponse> createBook(@RequestBody BookRequest bookRequest) {
+    LOGGER.info("Request to create book: {}", bookRequest);
+    var bookDto = domainMapper.convertRequestToDto(bookRequest, BookDto.class);
+    var savedBookDto = bookService.save(bookDto);
+
+    URI location =
+        ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(savedBookDto.id())
+            .toUri();
+    LOGGER.info("Book created successfully: {}", savedBookDto);
+    var bookResponse = domainMapper.convertDtoToResponse(savedBookDto, BookResponse.class);
+    return ResponseEntity.created(location).body(bookResponse);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<BookResponse> updateBook(
+      @PathVariable Long id, @RequestBody BookDto bookDto) {
+    LOGGER.info("Request to update book with ID: {}", id);
+    if (!bookService.isExists(id)) {
+      LOGGER.error("Attempted to update a non-existent book with ID: {}", id);
+      return ResponseEntity.notFound().build();
     }
+    var updatedBookDto = bookService.partialUpdate(id, bookDto);
+    LOGGER.info("Book updated successfully: {}", updatedBookDto);
+    var bookResponse = domainMapper.convertDtoToResponse(updatedBookDto, BookResponse.class);
+    return ResponseEntity.ok(bookResponse);
+  }
 
-    /**
-     * Creates a new book entry in the database.
-     * @param bookDto the book DTO to create a new book.
-     * @return ResponseEntity containing the created book.
-     */
-    @PostMapping
-    public ResponseEntity<BookDto> createBook(@RequestBody BookDto bookDto) {
-        LOGGER.info("Request to create book: {}", bookDto);
-        BookEntity bookEntity = bookModelMapper.mapTo(bookDto);
-        BookEntity savedBook = bookService.save(bookEntity);
-        BookDto savedBookDto = bookModelMapper.mapFrom(savedBook);
-
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(savedBook.getId())
-                .toUri();
-
-        LOGGER.info("Book created successfully: {}", savedBookDto);
-        return ResponseEntity
-                .created(location)
-                .body(savedBookDto);
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+    LOGGER.info("Request to delete book with ID: {}", id);
+    if (!bookService.isExists(id)) {
+      LOGGER.error("Attempted to delete a non-existent book with ID: {}", id);
+      return ResponseEntity.notFound().build();
     }
-
-    /**
-     * Retrieves a book by its ID.
-     * @param id the ID of the book to retrieve.
-     * @return ResponseEntity containing the requested book.
-     */
-    @GetMapping("/{id}")
-    public ResponseEntity<BookDto> getBookById(@PathVariable Long id) {
-        LOGGER.info("Request to get book by ID: {}", id);
-        BookEntity book = bookService.findById(id)
-                .orElseThrow(() -> {
-            LOGGER.error("Book not found with ID: {}", id);
-            return new ResponseStatusException(HttpStatus.NOT_FOUND, "Book not found with ID: " + id);
-        });
-
-        BookDto bookDto = bookModelMapper.mapFrom(book);
-        LOGGER.info("Book retrieved successfully: {}", bookDto);
-        return ResponseEntity.ok(bookDto);
-    }
-
-    /**
-     * Retrieves all books with pagination.
-     * @param pageable the pagination information.
-     * @return ResponseEntity containing a page of books.
-     */
-    @GetMapping
-    public ResponseEntity<Page<BookDto>> getAllBooks(@PageableDefault(size = 10) Pageable pageable) {
-        LOGGER.info("Request to get all books");
-        Page<BookEntity> bookPage = bookService.findAll(pageable);
-        Page<BookDto> bookDtoPage = bookPage.map(bookModelMapper::mapFrom);
-        LOGGER.info("Books retrieved with pagination");
-        return ResponseEntity.ok(bookDtoPage);
-    }
-
-    /**
-     * Updates an existing book by ID.
-     * @param id the ID of the book to update.
-     * @param bookDto the updated book DTO.
-     * @return ResponseEntity containing the updated book.
-     */
-    @PutMapping("/{id}")
-    public ResponseEntity<BookDto> updateBook(@PathVariable Long id, @RequestBody BookDto bookDto) {
-        LOGGER.info("Request to update book with ID: {}", id);
-        if (bookService.isExists(id)) {
-            LOGGER.error("Attempted to update a non-existent book with ID: {}", id);
-            return ResponseEntity.notFound().build();
-        }
-        bookDto.setId(id);
-        BookEntity updatedBook = bookService.partialUpdate(id, bookModelMapper.mapTo(bookDto));
-        LOGGER.info("Book updated successfully: {}", updatedBook);
-        return ResponseEntity
-                .ok(bookModelMapper.mapFrom(updatedBook));
-    }
-
-    /**
-     * Deletes a book by its ID.
-     * @param id the ID of the book to delete.
-     * @return ResponseEntity indicating success or failure.
-     */
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
-        LOGGER.info("Request to delete book with ID: {}", id);
-        if (bookService.isExists(id)) {
-            LOGGER.error("Attempted to delete a non-existent book with ID: {}", id);
-            return ResponseEntity.notFound().build();
-        }
-        bookService.delete(id);
-        LOGGER.info("Book deleted successfully with ID: {}", id);
-        return ResponseEntity
-                .noContent()
-                .build();
-    }
+    bookService.delete(id);
+    LOGGER.info("Book deleted successfully with ID: {}", id);
+    return ResponseEntity.noContent().build();
+  }
 }
--- src/main/java/io/bartmilo/student/enrolment/app/domain/book/service/BookServiceImpl.java
+++ src/main/java/io/bartmilo/student/enrolment/app/domain/book/service/BookServiceImpl.java
@@ -1,81 +1,100 @@
 package io.bartmilo.student.enrolment.app.domain.book.service;
 
+import io.bartmilo.student.enrolment.app.domain.book.exception.BookNotFoundException;
+import io.bartmilo.student.enrolment.app.domain.book.model.BookDto;
 import io.bartmilo.student.enrolment.app.domain.book.model.BookEntity;
 import io.bartmilo.student.enrolment.app.domain.book.repository.BookRepository;
+import io.bartmilo.student.enrolment.app.util.DomainMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
 @Service
 public class BookServiceImpl implements BookService {
-    private static final Logger LOGGER = LoggerFactory.getLogger(BookServiceImpl.class);
-    private final BookRepository bookRepository;
+  private static final Logger LOGGER = LoggerFactory.getLogger(BookServiceImpl.class);
+  private final BookRepository bookRepository;
+  private final DomainMapper domainMapper;
 
-    @Autowired
-    public BookServiceImpl(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
+  public BookServiceImpl(BookRepository bookRepository, DomainMapper domainMapper) {
+    this.bookRepository = bookRepository;
+    this.domainMapper = domainMapper;
+  }
+
+  @Override
+  @Transactional
+  public BookDto save(BookDto bookDto) {
+    LOGGER.info("Saving book to the database: {}", bookDto);
+    var bookEntity = domainMapper.convertDtoToEntity(bookDto, BookEntity.class);
+    var savedBookEntity = bookRepository.save(bookEntity);
+    return domainMapper.convertEntityToDto(savedBookEntity, BookDto.class);
+  }
+
+  @Override
+  @Transactional(readOnly = true)
+  public Page<BookDto> findAll(Pageable pageable) {
+    LOGGER.info("Fetching all books with pagination");
+    var bookEntityPage = bookRepository.findAll(pageable);
+    return bookEntityPage.map(entity -> domainMapper.convertEntityToDto(entity, BookDto.class));
+  }
+
+  @Override
+  @Transactional(readOnly = true)
+  public BookDto findById(Long id) {
+    LOGGER.info("Finding book with ID: {}", id);
+    return bookRepository
+        .findById(id)
+        .map(bookEntity -> domainMapper.convertEntityToDto(bookEntity, BookDto.class))
+        .orElseThrow(() -> new BookNotFoundException("Book not found with ID: " + id));
+  }
+
+  @Override
+  @Transactional(readOnly = true)
+  public boolean isExists(Long id) {
+    LOGGER.info("Checking whether the book exists with ID: {}", id);
+    return bookRepository.existsById(id);
+  }
+
+  @Override
+  @Transactional
+  public BookDto partialUpdate(Long id, BookDto bookDto) {
+    LOGGER.info("Updating book with ID: {}", id);
+    return bookRepository
+        .findById(id)
+        .map(existingBook -> updateExistingBook(bookDto, existingBook))
+        .orElseThrow(() -> new BookNotFoundException("Book not found with ID: " + id));
+  }
+
+  @Override
+  @Transactional
+  public void delete(Long id) {
+    LOGGER.info("Deleting book with ID: {}", id);
+    bookRepository.deleteById(id);
+  }
+
+  private BookDto updateExistingBook(BookDto bookDto, BookEntity existingBook) {
+    LOGGER.info("Found book to update: {}", existingBook);
+
+    if (bookDto.bookAuthor() != null) {
+      existingBook.setBookAuthor(bookDto.bookAuthor());
+    }
+    if (bookDto.title() != null) {
+      existingBook.setTitle(bookDto.title());
+    }
+    if (bookDto.isbn() != null) {
+      existingBook.setIsbn(bookDto.isbn());
+    }
+    if (bookDto.createdAt() != null) {
+      existingBook.setCreatedAt(bookDto.createdAt());
+    }
+    if (bookDto.stock() != null) {
+      existingBook.setStock(bookDto.stock());
     }
 
-    @Override
-    @Transactional
-    public BookEntity save(BookEntity bookEntity) {
-        LOGGER.info("Saving book to the database: {}", bookEntity);
-        return bookRepository.save(bookEntity);
-    }
-
-    @Override
-    @Transactional(readOnly = true)
-    public Page<BookEntity> findAll(Pageable pageable) {
-        LOGGER.info("Fetching all books with pagination");
-        return bookRepository.findAll(pageable);
-    }
-
-    @Override
-    @Transactional(readOnly = true)
-    public Optional<BookEntity> findById(Long id) {
-        LOGGER.info("Finding book with ID: {}", id);
-        return bookRepository.findById(id);
-    }
-
-    @Override
-    @Transactional(readOnly = true)
-    public boolean isExists(Long id) {
-        LOGGER.info("Checking whether the book exists with ID: {}", id);
-        return !bookRepository.existsById(id);
-    }
-
-    @Override
-    @Transactional
-    public BookEntity partialUpdate(Long id, BookEntity bookEntity) {
-        LOGGER.info("Make sure the book you want to update has specified id: {}", id);
-        bookEntity.setId(id);
-
-        LOGGER.info("Update book: {}, with id: {}", bookEntity, id);
-        return bookRepository.findById(id).map(existingBook -> {
-            Optional.ofNullable(bookEntity.getBookAuthor())
-                    .ifPresent(existingBook::setBookAuthor);
-            Optional.ofNullable(bookEntity.getTitle())
-                    .ifPresent(existingBook::setTitle);
-            Optional.ofNullable(bookEntity.getIsbn())
-                    .ifPresent(existingBook::setIsbn);
-            Optional.ofNullable(bookEntity.getCreatedAt())
-                    .ifPresent(existingBook::setCreatedAt);
-            Optional.ofNullable(bookEntity.getStock())
-                    .ifPresent(existingBook::setStock);
-            return bookRepository.save(existingBook);
-        }).orElseThrow(() -> new RuntimeException("Book does not exist"));
-    }
-
-    @Override
-    @Transactional
-    public void delete(Long id) {
-        LOGGER.info("Deleting book with ID: {}", id);
-        bookRepository.deleteById(id);
-    }
+    var updatedBook = bookRepository.save(existingBook);
+    LOGGER.info("Updated book: {}", updatedBook);
+    return domainMapper.convertEntityToDto(updatedBook, BookDto.class);
+  }
 }
