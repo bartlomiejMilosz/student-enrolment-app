3e037fb8a71a82ab00c1f86f1b010d17916d4b56
Bart Milo <bartlomiej.milosz@gmail.com>
2024-05-22 08:15:47 +0200

    Refactor BookEntity by using more uniform google-formater and delete error-prone lombok @Data annotation

--- src/main/java/io/bartmilo/student/enrolment/app/domain/book/model/BookEntity.java
+++ src/main/java/io/bartmilo/student/enrolment/app/domain/book/model/BookEntity.java
@@ -2,13 +2,13 @@ package io.bartmilo.student.enrolment.app.domain.book.model;
 
 import io.bartmilo.student.enrolment.app.domain.rental.model.RentalEntity;
 import jakarta.persistence.*;
-import lombok.*;
-
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
 @ToString(exclude = "rentalEntityList")
 @EqualsAndHashCode(exclude = "rentalEntityList")
 @NoArgsConstructor
@@ -16,78 +16,35 @@ import java.util.List;
 @Builder
 @Entity(name = "Book")
 @Table(
-        name = "book",
-        uniqueConstraints = {
-                @UniqueConstraint(
-                        name = "title_unique",
-                        columnNames = "title"
-                ),
-                @UniqueConstraint(
-                        name = "isbn_unique",
-                        columnNames = "isbn"
-                )
-        }
-)
+    name = "book",
+    uniqueConstraints = {
+      @UniqueConstraint(name = "title_unique", columnNames = "title"),
+      @UniqueConstraint(name = "isbn_unique", columnNames = "isbn")
+    })
 public class BookEntity {
-    @Id
-    @SequenceGenerator(
-            name = "book_sequence",
-            sequenceName = "book_sequence",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "book_sequence"
-    )
-    @Column(
-            name = "id",
-            updatable = false
-    )
-    private Long id;
+  @Id
+  @SequenceGenerator(name = "book_sequence", sequenceName = "book_sequence", allocationSize = 1)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "book_sequence")
+  @Column(name = "id", updatable = false)
+  private Long id;
 
-    @Column(
-            name = "book_author",
-            nullable = false,
-            columnDefinition = "TEXT"
-    )
-    private String bookAuthor;
+  @Column(name = "book_author", nullable = false, columnDefinition = "TEXT")
+  private String bookAuthor;
 
-    @Column(
-            name = "title",
-            nullable = false,
-            columnDefinition = "TEXT"
-    )
-    private String title;
+  @Column(name = "title", nullable = false, columnDefinition = "TEXT")
+  private String title;
 
-    @Column(
-            name = "isbn",
-            nullable = false,
-            columnDefinition = "TEXT"
-    )
-    private String isbn;
+  @Column(name = "isbn", nullable = false, columnDefinition = "TEXT")
+  private String isbn;
 
-    @Column(
-            name = "created_at",
-            nullable = false,
-            columnDefinition = "TIMESTAMP WITHOUT TIME ZONE"
-    )
-    private LocalDateTime createdAt;
+  @Column(name = "created_at", nullable = false, columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+  private LocalDateTime createdAt;
 
-    @Column(
-            name = "stock",
-            nullable = true // stock can be null
-    )
-    private Integer stock;
+  @Column(name = "stock")
+  private Integer stock;
 
-    /* RELATIONS */
+  /* RELATIONS */
 
-    @OneToMany(
-            mappedBy = "bookEntity",
-            orphanRemoval = true
-           /* cascade = {
-                    CascadeType.PERSIST,
-                    CascadeType.REMOVE
-            }*/
-    )
-    private List<RentalEntity> rentalEntityList = new ArrayList<>();
+  @OneToMany(mappedBy = "bookEntity", orphanRemoval = true)
+  private List<RentalEntity> rentalEntityList = new ArrayList<>();
 }
