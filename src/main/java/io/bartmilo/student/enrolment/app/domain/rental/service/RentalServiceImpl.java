b639708c14f860bbf1566a8f2d637cf761310a2b
Bart Milo <bartlomiej.milosz@gmail.com>
2024-05-22 11:13:00 +0200

    Refactor service and decouple it by providing Dto model

--- src/main/java/io/bartmilo/student/enrolment/app/domain/rental/service/RentalServiceImpl.java
+++ src/main/java/io/bartmilo/student/enrolment/app/domain/rental/service/RentalServiceImpl.java
@@ -1,105 +1,130 @@
 package io.bartmilo.student.enrolment.app.domain.rental.service;
 
+import io.bartmilo.student.enrolment.app.domain.book.model.BookDto;
 import io.bartmilo.student.enrolment.app.domain.book.model.BookEntity;
-import io.bartmilo.student.enrolment.app.domain.rental.model.RentalEntity;
-import io.bartmilo.student.enrolment.app.domain.student.model.StudentEntity;
-import io.bartmilo.student.enrolment.app.domain.student.model.StudentIdCardEntity;
-import io.bartmilo.student.enrolment.app.domain.rental.repository.RentalRepository;
 import io.bartmilo.student.enrolment.app.domain.book.service.BookService;
+import io.bartmilo.student.enrolment.app.domain.rental.exception.StudentNotFoundException;
+import io.bartmilo.student.enrolment.app.domain.rental.model.RentalDto;
+import io.bartmilo.student.enrolment.app.domain.rental.model.RentalEntity;
+import io.bartmilo.student.enrolment.app.domain.rental.repository.RentalRepository;
+import io.bartmilo.student.enrolment.app.domain.student.model.StudentIdCardEntity;
 import io.bartmilo.student.enrolment.app.domain.student.service.StudentService;
+import io.bartmilo.student.enrolment.app.util.DomainMapper;
 import jakarta.persistence.EntityNotFoundException;
+import java.time.LocalDateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
-
 @Service
 public class RentalServiceImpl implements RentalService {
-    private static final Logger LOGGER = LoggerFactory.getLogger(RentalServiceImpl.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(RentalServiceImpl.class);
 
-    private final StudentService studentService;
-    private final BookService bookService;
-    private final RentalRepository rentalRepository;
+  private final StudentService studentService;
+  private final BookService bookService;
+  private final RentalRepository rentalRepository;
+  private final DomainMapper domainMapper;
 
-    public RentalServiceImpl(
-            StudentService studentService,
-            BookService bookService,
-            RentalRepository rentalRepository
-    ) {
-        this.studentService = studentService;
-        this.bookService = bookService;
-        this.rentalRepository = rentalRepository;
+  public RentalServiceImpl(
+      StudentService studentService,
+      BookService bookService,
+      RentalRepository rentalRepository,
+      DomainMapper domainMapper) {
+    this.studentService = studentService;
+    this.bookService = bookService;
+    this.rentalRepository = rentalRepository;
+    this.domainMapper = domainMapper;
+  }
+
+  @Override
+  @Transactional
+  public RentalDto rentBook(Long bookId, Long studentId, LocalDateTime dueDate) {
+    LOGGER.info(
+        "Attempting to rent book with ID: {} for student with ID: {} due by {}",
+        bookId,
+        studentId,
+        dueDate);
+    var studentEntity =
+        studentService
+            .findById(studentId)
+            .orElseThrow(
+                () -> new StudentNotFoundException("Student not found with ID: " + studentId));
+    LOGGER.info("Student details retrieved: {}", studentEntity);
+
+    // Check the status of the student's ID card
+    if (studentEntity.getStudentIdCardEntity() == null
+        || studentEntity.getStudentIdCardEntity().getStatus()
+            != StudentIdCardEntity.CardStatus.ACTIVE) {
+      LOGGER.warn("Attempt to rent with inactive ID card by student ID: {}", studentId);
+      throw new IllegalStateException("Student's ID card is not active.");
     }
 
-    @Override
-    @Transactional
-    public RentalEntity rentBook(Long bookId, Long studentId, LocalDateTime dueDate) {
-        LOGGER.info(
-                "Attempting to rent book with ID: {} for student with ID: {} due by {}",
-                bookId,
-                studentId,
-                dueDate
-        );
-        StudentEntity student = studentService.findById(studentId)
-                .orElseThrow(() -> new EntityNotFoundException("Student not found with ID: " + studentId));
-        LOGGER.debug("Student details retrieved: {}", student);
+    var bookDto = bookService.findById(bookId);
+    LOGGER.info("Book details retrieved: {}", bookDto);
 
-        // Check the status of the student's ID card
-        if (
-                student.getStudentIdCardEntity() == null ||
-                student.getStudentIdCardEntity().getStatus() != StudentIdCardEntity.CardStatus.ACTIVE
-        ) {
-            LOGGER.warn("Attempt to rent with inactive ID card by student ID: {}", studentId);
-            throw new IllegalStateException("Student's ID card is not active.");
-        }
-
-        BookEntity book = bookService.findById(bookId)
-                .orElseThrow(() -> new EntityNotFoundException("Book not found with ID: " + bookId));
-        LOGGER.debug("Book details retrieved: {}", book);
-
-        if (book.getStock() == null || book.getStock() <= 0) {
-            LOGGER.warn("Book stock unavailable for book ID: {}", bookId);
-            throw new IllegalArgumentException("Book is not available for rent.");
-        }
-
-        book.setStock(book.getStock() - 1); // Decrement the stock
-        bookService.save(book); // Save the updated book stock
-        LOGGER.info("Updated book stock for book ID: {}. New stock: {}", bookId, book.getStock());
-
-        RentalEntity rentalEntity = new RentalEntity();
-        rentalEntity.setRentedAt(LocalDateTime.now());
-        rentalEntity.setBookEntity(book);
-        rentalEntity.setStudentEntity(student);
-        rentalEntity.setDueDate(dueDate);
-        LOGGER.debug("Created rental record: {}", rentalEntity);
-
-        LOGGER.info("Saving rental to the database.");
-        return rentalRepository.save(rentalEntity);
+    if (bookDto.stock() == null || bookDto.stock() <= 0) {
+      LOGGER.error("Book stock unavailable for book ID: {}", bookId);
+      throw new IllegalArgumentException("Book is not available for rent.");
     }
 
-    @Override
-    @Transactional
-    public RentalEntity returnBook(Long rentalId) {
-        var rental = rentalRepository.findById(rentalId)
-                .orElseThrow(() -> new EntityNotFoundException("Rental not found with ID: " + rentalId));
+    var rentedBookDto =
+        new BookDto(
+            bookDto.id(),
+            bookDto.bookAuthor(),
+            bookDto.title(),
+            bookDto.isbn(),
+            bookDto.createdAt(),
+            bookDto.stock() - 1);
+    bookService.save(rentedBookDto); // Save the updated book stock
+    LOGGER.info("Updated book stock for book ID: {}. New stock: {}", bookId, rentedBookDto.stock());
 
-        if (rental.getReturnedAt() != null) {
-            throw new IllegalStateException("This book has already been returned.");
-        }
+    var rentedBookEntity = domainMapper.convertDtoToEntity(rentedBookDto, BookEntity.class);
 
-        LOGGER.info(String.valueOf(rental.getBookEntity().getStock()));
-        var book = rental.getBookEntity();
-        book.setStock(book.getStock() + 1);  // Increment stock on return
-        bookService.save(book);
-        LOGGER.info(String.valueOf(book.getStock()));
+    var rentalEntity = new RentalEntity();
+    rentalEntity.setRentedAt(LocalDateTime.now());
+    rentalEntity.setBookEntity(rentedBookEntity);
+    rentalEntity.setStudentEntity(studentEntity);
+    rentalEntity.setDueDate(dueDate);
+    LOGGER.info("Created rental record: {}", rentalEntity);
 
-        rental.setReturnedAt(LocalDateTime.now());
-        rentalRepository.save(rental);
+    LOGGER.info("Saving rental to the database.");
 
-        LOGGER.info("Book returned successfully: {}", rental);
-        return rental;
+    var savedRentalEntity = rentalRepository.save(rentalEntity);
+    return domainMapper.convertEntityToDto(savedRentalEntity, RentalDto.class);
+  }
+
+  @Override
+  @Transactional
+  public RentalDto returnBook(Long rentalId) {
+    var rentalEntity =
+        rentalRepository
+            .findById(rentalId)
+            .orElseThrow(
+                () -> new EntityNotFoundException("Rental not found with ID: " + rentalId));
+
+    if (rentalEntity.getReturnedAt() != null) {
+      throw new IllegalStateException("This book has already been returned.");
     }
 
+    LOGGER.info(String.valueOf(rentalEntity.getBookEntity().getStock()));
+    var bookEntity = rentalEntity.getBookEntity();
+    var returnedBookDto =
+        new BookDto(
+            bookEntity.getId(),
+            bookEntity.getBookAuthor(),
+            bookEntity.getTitle(),
+            bookEntity.getIsbn(),
+            bookEntity.getCreatedAt(),
+            bookEntity.getStock() + 1);
+
+    bookService.save(returnedBookDto);
+    LOGGER.info(String.valueOf(returnedBookDto.stock()));
+
+    rentalEntity.setReturnedAt(LocalDateTime.now());
+    rentalRepository.save(rentalEntity);
+
+    LOGGER.info("Book returned successfully: {}", rentalEntity);
+    return domainMapper.convertEntityToDto(rentalEntity, RentalDto.class);
+  }
 }
