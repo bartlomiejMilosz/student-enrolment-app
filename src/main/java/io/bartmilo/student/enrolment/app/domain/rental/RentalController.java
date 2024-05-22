9d23258989e61b83a046406492dbf4c4cbd845f7
Bart Milo <bartlomiej.milosz@gmail.com>
2024-05-22 11:13:29 +0200

    Refactor controller and reduce redundant comments

--- src/main/java/io/bartmilo/student/enrolment/app/domain/rental/RentalController.java
+++ src/main/java/io/bartmilo/student/enrolment/app/domain/rental/RentalController.java
@@ -1,83 +1,56 @@
 package io.bartmilo.student.enrolment.app.domain.rental;
 
-import io.bartmilo.student.enrolment.app.domain.rental.model.RentalDto;
-import io.bartmilo.student.enrolment.app.domain.rental.model.RentalEntity;
-import io.bartmilo.student.enrolment.app.util.DomainMapper;
-import io.bartmilo.student.enrolment.app.domain.rental.mapper.RentalModelMapper;
+import io.bartmilo.student.enrolment.app.domain.rental.model.RentalRequest;
+import io.bartmilo.student.enrolment.app.domain.rental.model.RentalResponse;
 import io.bartmilo.student.enrolment.app.domain.rental.service.RentalService;
+import io.bartmilo.student.enrolment.app.util.DomainMapper;
+import java.net.URI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
-
 @RestController
 @RequestMapping("/rentals")
 public class RentalController {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(RentalController.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(RentalController.class);
 
-    private final RentalService rentalService;
-    private final DomainMapper<RentalEntity, RentalDto> rentalModelMapper;
+  private final RentalService rentalService;
+  private final DomainMapper domainMapper;
 
-    @Autowired
-    public RentalController(RentalService rentalService, RentalModelMapper rentalMapper) {
-        this.rentalService = rentalService;
-        this.rentalModelMapper = rentalMapper;
-    }
+  public RentalController(RentalService rentalService, DomainMapper domainMapper) {
+    this.rentalService = rentalService;
+    this.domainMapper = domainMapper;
+  }
 
-    /**
-     * Endpoint to rent a book.
-     * @param rentalDto Contains the 'dueDate' in 'yyyy-MM-dd HH:mm' format.
-     * @return The rental details.
-     */
-    @PostMapping
-    public ResponseEntity<RentalDto> rentBook(@RequestBody RentalDto rentalDto) {
-        LOGGER.info("Request to rent book: {}", rentalDto);
-        var rentalEntity = rentalService.rentBook(
-                rentalDto.getBookId(),
-                rentalDto.getStudentId(),
-                rentalDto.getDueDate()
-        );
+  @PostMapping
+  public ResponseEntity<RentalResponse> rentBook(@RequestBody RentalRequest rentalRequest) {
+    LOGGER.info("Request to rent book: {}", rentalRequest);
+    var rentalDto =
+        rentalService.rentBook(
+            rentalRequest.bookId(), rentalRequest.studentId(), rentalRequest.dueDate());
 
-        var savedRentalDto = rentalModelMapper.mapFrom(rentalEntity);
+    URI location =
+        ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(rentalDto.id())
+            .toUri();
+    LOGGER.info("Rental created successfully: {}", rentalDto);
+    var rentalResponse = domainMapper.convertDtoToResponse(rentalDto, RentalResponse.class);
+    return ResponseEntity.created(location).body(rentalResponse);
+  }
 
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(rentalEntity.getId())
-                .toUri();
+  @PutMapping("/{id}")
+  public ResponseEntity<RentalResponse> returnBook(@PathVariable Long id) {
+    LOGGER.info("Initiating return process for rental ID: {}", id);
 
-        LOGGER.info("Rental created successfully: {}", savedRentalDto);
-        return ResponseEntity
-                .created(location)
-                .body(savedRentalDto);
-    }
+    var rentalDto = rentalService.returnBook(id);
 
-    /**
-     * Handles the HTTP PUT request to return a book that has been previously rented.
-     *
-     * @param id the unique identifier of the rental record to be updated as returned
-     * @return ResponseEntity<RentalDto> which contains the updated rental details after the book is returned.
-     * If successful, the HTTP status is set to OK and includes the updated RentalDto. If any exceptions
-     * are encountered (e.g., rental not found or book has already been returned), appropriate HTTP status codes
-     * and messages are returned.
-     */
-    @PutMapping("/{id}")
-    public ResponseEntity<RentalDto> returnBook(@PathVariable Long id) {
-        LOGGER.info("Initiating return process for rental ID: {}", id);
-
-        var rentalEntity = rentalService.returnBook(id);
-        var rentalDto = rentalModelMapper.mapFrom(rentalEntity);
-
-        LOGGER.info("Book returned successfully for rental ID: {}", id);
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(rentalDto);
-    }
+    LOGGER.info("Book returned successfully for rental ID: {}", id);
+    var rentalResponse = domainMapper.convertDtoToResponse(rentalDto, RentalResponse.class);
+    return ResponseEntity.status(HttpStatus.OK).body(rentalResponse);
+  }
 }
-
