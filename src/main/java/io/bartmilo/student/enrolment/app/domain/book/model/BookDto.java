127108ddbaa5368beccdbd530aafe5824f39d6eb
Bart Milo <bartlomiej.milosz@gmail.com>
2024-05-22 08:16:06 +0200

    Refactor BookDto to record

--- src/main/java/io/bartmilo/student/enrolment/app/domain/book/model/BookDto.java
+++ src/main/java/io/bartmilo/student/enrolment/app/domain/book/model/BookDto.java
@@ -1,27 +1,13 @@
 package io.bartmilo.student.enrolment.app.domain.book.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
+import lombok.*;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
-public class BookDto {
-
-    private Long id;
-
-    private String bookAuthor;
-
-    private String title;
-
-    private String isbn;
-
-    private LocalDateTime createdAt;
-
-    private Integer stock;
-}
+public record BookDto(
+    Long id,
+    String bookAuthor,
+    String title,
+    String isbn,
+    LocalDateTime createdAt,
+    Integer stock) {}
