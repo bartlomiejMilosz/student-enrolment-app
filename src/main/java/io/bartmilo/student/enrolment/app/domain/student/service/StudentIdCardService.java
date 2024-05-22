691f847ff87595b2059b5346d8e508a5686522e8
Bart Milo <bartlomiej.milosz@gmail.com>
2024-05-22 13:19:50 +0200

    Manage service interfaces and make them work on Dto objects

--- src/main/java/io/bartmilo/student/enrolment/app/domain/student/service/StudentIdCardService.java
+++ src/main/java/io/bartmilo/student/enrolment/app/domain/student/service/StudentIdCardService.java
@@ -1,18 +1,18 @@
 package io.bartmilo.student.enrolment.app.domain.student.service;
 
-import io.bartmilo.student.enrolment.app.domain.student.model.StudentEntity;
-import io.bartmilo.student.enrolment.app.domain.student.model.StudentIdCardEntity;
+import io.bartmilo.student.enrolment.app.domain.student.model.StudentDto;
+import io.bartmilo.student.enrolment.app.domain.student.model.StudentIdCardDto;
 
 public interface StudentIdCardService {
 
-    /**
-     * Generates a new ID card for the given student. This method is responsible for creating
-     * a new {@link StudentIdCardEntity} that represents an ID card issued to a student.
-     * The ID card contains details such as the card's unique identifier, issue date, and status.
-     *
-     * @param studentEntity the {@link StudentEntity} for whom the ID card is to be generated
-     * @return a {@link StudentIdCardEntity} representing the newly created student ID card,
-     *         which includes all relevant details such as card number, issue date, and status
-     */
-    StudentIdCardEntity generateStudentIdCard(StudentEntity studentEntity);
+  /**
+   * Generates a new ID card for the given student. This method is responsible for creating a new
+   * {@link StudentIdCardDto} that represents an ID card issued to a student. The ID card contains
+   * details such as the card's unique identifier, issue date, and status.
+   *
+   * @param studentDto the {@link StudentDto} for whom the ID card is to be generated
+   * @return a {@link StudentIdCardDto} representing the newly created student ID card, which
+   *     includes all relevant details such as card number, issue date, and status
+   */
+  StudentIdCardDto generateStudentIdCard(StudentDto studentDto);
 }
--- src/main/java/io/bartmilo/student/enrolment/app/domain/student/service/StudentService.java
+++ src/main/java/io/bartmilo/student/enrolment/app/domain/student/service/StudentService.java
@@ -1,56 +1,57 @@
 package io.bartmilo.student.enrolment.app.domain.student.service;
 
+import io.bartmilo.student.enrolment.app.domain.student.model.StudentDto;
 import io.bartmilo.student.enrolment.app.domain.student.model.StudentEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.Optional;
-
 public interface StudentService {
 
-    /**
-     * Saves the specified student to the database.
-     *
-     * @param studentEntity The student to save to database.
-     * @return The saved student.
-     */
-    StudentEntity save(StudentEntity studentEntity);
+  /**
+   * Saves the specified student to the database.
+   *
+   * @param studentDto The student to save to database.
+   * @return The saved student.
+   */
+  StudentDto save(StudentDto studentDto);
 
-    /**
-     * Returns all students in the database.
-     *
-     * @return All students in the database.
-     */
-    Page<StudentEntity> findAll(Pageable pageable);
+  /**
+   * Returns all students in the database.
+   *
+   * @return All students in the database.
+   */
+  Page<StudentDto> findAll(Pageable pageable);
 
-    /**
-     * Returns the student with the specified id.
-     *
-     * @param id ID of the student to retrieve.
-     * @return The requested student if found.
-     */
-    Optional<StudentEntity> findById(Long id);
+  /**
+   * Returns the student with the specified id.
+   *
+   * @param id ID of the student to retrieve.
+   * @return The requested student if found.
+   */
+  StudentDto findById(Long id);
 
-    /**
-     * Returns boolean of student in the database with the specified ID. Checks whether the student exists in the database.
-     * @param id    ID of the student to check.
-     * @return      The requested student existence.
-     */
-    boolean isExists(Long id);
+  /**
+   * Returns boolean of student in the database with the specified ID. Checks whether the student
+   * exists in the database.
+   *
+   * @param id ID of the student to check.
+   * @return The requested student existence.
+   */
+  boolean exists(Long id);
 
-    /**
-     * Updates the specified student, identified by given ID.
-     *
-     * @param id            The ID of the student to update.
-     * @param studentEntity The student to update.
-     * @return The updated student.
-     */
-    StudentEntity partialUpdate(Long id, StudentEntity studentEntity);
+  /**
+   * Updates the specified student, identified by given ID.
+   *
+   * @param id The ID of the student to update.
+   * @param studentDto The student to update.
+   * @return The updated student.
+   */
+  StudentDto partialUpdate(Long id, StudentDto studentDto);
 
-    /**
-     * Deletes the student with the specified ID.
-     *
-     * @param id The ID of the student to delete.
-     */
-    void delete(Long id);
+  /**
+   * Deletes the student with the specified ID.
+   *
+   * @param id The ID of the student to delete.
+   */
+  void delete(Long id);
 }
