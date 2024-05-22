351923536ae77e1cbc680535bf726eb278eaa04e
Bart Milo <bartlomiej.milosz@gmail.com>
2024-05-22 08:17:54 +0200

    Extend book repository by simply adding JpaRepository

--- src/main/java/io/bartmilo/student/enrolment/app/domain/book/repository/BookRepository.java
+++ src/main/java/io/bartmilo/student/enrolment/app/domain/book/repository/BookRepository.java
@@ -1,12 +1,8 @@
 package io.bartmilo.student.enrolment.app.domain.book.repository;
 
 import io.bartmilo.student.enrolment.app.domain.book.model.BookEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository extends
-        CrudRepository<BookEntity, Long>,
-        PagingAndSortingRepository<BookEntity, Long> {
-}
+public interface BookRepository extends JpaRepository<BookEntity, Long> {}
