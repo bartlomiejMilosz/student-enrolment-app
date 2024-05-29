d1c1cc68730a6a9eb8d845b0d7b5c97c8248c726
Bart Milo <bartlomiej.milosz@gmail.com>
2024-05-29 17:22:42 +0200

    Change BookDto record to class object and remove immutability of created objects

--- src/main/java/io/bartmilo/student/enrolment/app/domain/book/model/BookDto.java
+++ src/main/java/io/bartmilo/student/enrolment/app/domain/book/model/BookDto.java
@@ -3,11 +3,18 @@ package io.bartmilo.student.enrolment.app.domain.book.model;
 import java.time.LocalDateTime;
 import lombok.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
-public record BookDto(
-    Long id,
-    String bookAuthor,
-    String title,
-    String isbn,
-    LocalDateTime createdAt,
-    Integer stock) {}
+@EqualsAndHashCode
+@ToString
+public class BookDto {
+  private Long id;
+  private String bookAuthor;
+  private String title;
+  private String isbn;
+  private LocalDateTime createdAt;
+  private Integer stock;
+}
